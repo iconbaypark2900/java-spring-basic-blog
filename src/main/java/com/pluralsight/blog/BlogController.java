7797890eb7f3ee3ce68525687df3ebb5532f1eea
iconbaypark2900 <jonaston015@gmail.com>
2023-03-31 15:28:02 -0400

    fixed the postDetails method

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -25,7 +25,8 @@ public class BlogController {
         return "home";
     }
 
-      public String postDetails(@PathVariable Long id, ModelMap modelMap) {
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap modelMap) {
         Post post = postRepository.findById(id);
         modelMap.put("post", post);
         return "post-details";
